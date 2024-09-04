be299138f253b1bb5fb25d5932021e4c9160446c
shendrew <andrewsh190@gmail.com>
2024-09-04 16:54:11 -0400

    update license in code

--- extension.js
+++ extension.js
@@ -2,7 +2,7 @@
  * Copyright - 2024 Andrew Shen <github.com/shendrew>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * SPDX-License-Identifier: GPL-2.0-or-later
+ * SPDX-License-Identifier: GPL-3.0-or-later
  */
 import Gio from 'gi://Gio';
 import St from 'gi://St';
