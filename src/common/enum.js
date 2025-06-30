d07d8d8a01ce8ca2db59bda0257e90399449e244
shendrew <andrewsh190@gmail.com>
2025-06-29 23:56:45 -0400

    feat: dropdown ui for lilypad indicator display behavior

---
+++ src/common/enum.js
@@ -0,0 +1,6 @@
+export const HideExtension = {
+    NEVER: { value: 0, nick: 'Never' },
+    WHEN_EMPTY: { value: 1, nick: 'When empty' },
+    ALWAYS: { value: 2, nick: 'Always' },
+    nicks: ['Never', 'When empty', 'Always'],
+};
--- src/displayPage.js
+++ src/displayPage.js
@@ -1,8 +1,10 @@
 import Gio from 'gi://Gio';
 import Adw from "gi://Adw";
 import GObject from "gi://GObject";
+import Gtk from "gi://Gtk";
 import GLib from "gi://GLib";
 
+import { HideExtension } from "./common/enum.js";
 
 export default class DisplayPage extends Adw.PreferencesPage {
     static {
@@ -10,6 +12,7 @@ export default class DisplayPage extends Adw.PreferencesPage {
             GTypeName: "DisplayUI",
             Template: GLib.uri_resolve_relative(import.meta.url, "../ui/display.ui", null),
             InternalChildren: [
+                "hide-indicator-dropdown",
                 "auto-collapse-millisecond-spin-button",
                 "auto-collapse-switch",
             ]
@@ -21,9 +24,19 @@ export default class DisplayPage extends Adw.PreferencesPage {
         let {Settings, ...args} = params;
         super._init(args);
 
-
+        this._initHideIndicatorDropdown('hide-indicator');
         this._initAutoCollapseMillisecondSwitchButton('auto-collapse-millisecond');
         this._initAutoCollapseSwitch('auto-collapse');
+    }
+
+    _initHideIndicatorDropdown(keyname) {
+        this._hide_indicator_dropdown.set_model(
+            new Gtk.StringList({
+                strings: HideExtension.nicks
+            })
+        );
+        
+        this._settings.bind(keyname, this._hide_indicator_dropdown, "selected", Gio.SettingsBindFlags.DEFAULT)
     }
 
     _initAutoCollapseMillisecondSwitchButton(keyName) {
@@ -34,4 +47,8 @@ export default class DisplayPage extends Adw.PreferencesPage {
         this._settings.bind(keyName, this._auto_collapse_switch, 'active', Gio.SettingsBindFlags.DEFAULT);
     };
 
+    _emitReorder() {
+        const reorder_state = this._settings.get_boolean("reorder");
+        this._settings.set_boolean("reorder", reorder_state^1);
+    }
 }
