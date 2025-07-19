20f2176c7f8e6a842c37505d2d4c0ad69a84d088
shendrew <andrewsh190@gmail.com>
2025-07-19 15:35:02 -0400

    feat: add icon file selector in display settings

--- src/displayPage.js
+++ src/displayPage.js
@@ -15,6 +15,8 @@ export default class DisplayPage extends Adw.PreferencesPage {
                 "hide-indicator-dropdown",
                 "auto-collapse-millisecond-spin-button",
                 "auto-collapse-switch",
+                "select-closed-icon",
+                "select-open-icon",
             ]
         }, this);
     }
@@ -27,6 +29,7 @@ export default class DisplayPage extends Adw.PreferencesPage {
         this._initHideIndicatorDropdown('hide-indicator');
         this._initAutoCollapseMillisecondSwitchButton('auto-collapse-millisecond');
         this._initAutoCollapseSwitch('auto-collapse');
+        this._initFileSelectors("closed-icon-path", "open-icon-path");
     }
 
     _initHideIndicatorDropdown(keyname) {
@@ -45,5 +48,64 @@ export default class DisplayPage extends Adw.PreferencesPage {
 
     _initAutoCollapseSwitch(keyName) {
         this._settings.bind(keyName, this._auto_collapse_switch, 'active', Gio.SettingsBindFlags.DEFAULT);
+    }
+
+    _initFileSelectors(closedKey, openKey) {
+        // set label if settings are set
+        if (this._settings.get_string(closedKey))
+            this._select_closed_icon.set_label(GLib.path_get_basename(this._settings.get_string(closedKey)));
+        if (this._settings.get_string(openKey))
+            this._select_open_icon.set_label(GLib.path_get_basename(this._settings.get_string(openKey)));
+
+        this._select_closed_icon.connect('clicked', () => {
+            this._openIconFileDialog(closedKey, 'Select Closed Icon');
+        });
+
+        this._select_open_icon.connect('clicked', () => {
+            this._openIconFileDialog(openKey, 'Select Open Icon');
+        });
+    }
+
+    _openIconFileDialog(settingsKey, title) {
+        const dialog = new Gtk.FileDialog({
+            title: title,
+            modal: true,
+        });
+        const allowedPatterns = ['*.png', '*.jpg', '*.jpeg', '*.svg', '*.gif'];
+
+        // file filter for images
+        // GTK directory change might refresh filter, not sure if a workaround exists
+        const filter = new Gtk.FileFilter();
+        filter.set_name('Image files');
+        filter.add_mime_type('image/*');
+        allowedPatterns.forEach(pattern => filter.add_pattern(pattern));
+
+        const filterStore = new Gio.ListStore();
+        filterStore.append(filter);
+        dialog.set_filters(filterStore);
+        dialog.set_default_filter(filter);
+
+        const parent = this.get_root();
+
+        dialog.open(parent, null, (source, result) => {
+            try {
+                const file = dialog.open_finish(result);
+                if (!file) return;
+                const filePath = file.get_path();
+                if (!filePath) return;
+
+                this._settings.set_string(settingsKey, filePath);
+                
+                // update button label to show selected file name
+                const fileName = GLib.path_get_basename(filePath);
+                if (settingsKey === 'closed-icon-path') {
+                    this._select_closed_icon.set_label(fileName);
+                } else if (settingsKey === 'open-icon-path') {
+                    this._select_open_icon.set_label(fileName);
+                }
+            } catch (error) {
+                console.log('File selection cancelled or error:', error.message);
+            }
+        });
     }
 }
